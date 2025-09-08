a7f138a01cae9336b6b8248e9d1ac3766504bb51
MarilouAiko <e1082409@cmaisonneuve.qc.ca>
2025-09-08 09:44:32 -0400

    Calcul du score

--- score.py
+++ score.py
@@ -1,6 +1,8 @@
-# Calculer le score pour le Blackjack
+# Calculer le score
+
 def calculer_score(main):
-    score = sum(card.value for card in main)
-    if score > 21 and any(card.rank == 'A' for card in main):
+    score = sum(card.getValeur() for card in main)
+    if score > 21 and any(card.getChiffre() == 'A' for card in main):
         score -= 10
     return score
+
